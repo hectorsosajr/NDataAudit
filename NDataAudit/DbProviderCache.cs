294d6ae61616833315cd90e2238e77f64726f6a4
Hector Sosa, Jr <hectorsosajr@gmail.com>
2016-04-14 15:07:47 -0600

    Started adding multi-database support. Will be testing using MEF to load database providers.

---
+++ NDataAudit/DbProviderCache.cs
@@ -0,0 +1,48 @@
+﻿
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.ComponentModel.Composition.Hosting;
+using System.Reflection;
+
+namespace NDataAudit.Framework
+{
+    /// <summary>
+    /// Class #ctor.
+    /// </summary>
+    public class DbProviderCache
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DbProviderCache"/> class.
+        /// </summary>
+        public DbProviderCache()
+        {
+            this.PopulateProviderCache();
+        }
+
+        /// <summary>
+        /// Gets the available database providers.
+        /// </summary>
+        /// <value>The providers.</value>
+        public Dictionary<string, IAuditDbProvider> Providers { get; private set; }
+
+        [ImportMany(typeof(IAuditDbProvider))]
+        private List<IAuditDbProvider> DatabaseProviderCache { get; set; }
+
+        private void PopulateProviderCache()
+        {
+            if (this.Providers == null)
+            {
+                this.Providers = new Dictionary<string, IAuditDbProvider>();
+            }
+
+            var catalog = new AggregateCatalog(new DirectoryCatalog("."), new AssemblyCatalog(Assembly.GetExecutingAssembly()));
+            var container = new CompositionContainer(catalog);
+            container.ComposeParts(this);
+
+            foreach (IAuditDbProvider dbProvider in this.DatabaseProviderCache)
+            {
+                this.Providers.Add(dbProvider.ProviderNamespace.ToLowerInvariant(), dbProvider);
+            }
+        }
+    }
+}
---
+++ NDataAudit/IAuditDbProvider.cs
@@ -0,0 +1,34 @@
+﻿using System.Data;
+
+namespace NDataAudit.Framework
+{
+    /// <summary>
+    /// Interface IAuditDbProvider
+    /// </summary>
+    public interface IAuditDbProvider
+    {
+        /// <summary>
+        /// Gets or sets the connection string.
+        /// </summary>
+        /// <value>The connection string.</value>
+        string ConnectionString { get; set; }
+
+        /// <summary>
+        /// Gets the name of the database.
+        /// </summary>
+        /// <value>The name of the database.</value>
+        string DatabaseName { get; }
+
+        /// <summary>
+        /// Gets the provider namespace.
+        /// </summary>
+        /// <value>The provider namespace.</value>
+        string ProviderNamespace { get; }
+
+        /// <summary>
+        /// Creates the database session.
+        /// </summary>
+        /// <returns>IDbConnection.</returns>
+        IDbConnection CreateDatabaseSession();
+    }
+}
