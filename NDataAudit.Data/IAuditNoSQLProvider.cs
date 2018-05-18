304d6c18776a8c654a047a30ed51f9623049cd8b
Hector Sosa, Jr <hectorsosajr@gmail.com>
2018-05-17 19:36:10 -0600

    Started work on NoSQL engines.

---
+++ NDataAudit.Data/IAuditNoSQLProvider.cs
@@ -0,0 +1,45 @@
+﻿//*********************************************************************
+// File:       		IAuditNoSqlProvider.cs
+// Author:  	    Hector Sosa, Jr
+// Date:			5/17/2018
+//*********************************************************************
+// Change Log
+//*********************************************************************
+// USER					DATE            COMMENTS
+// Hector Sosa, Jr		5/17/2018	    Created
+//*********************************************************************
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NDataAudit.Data
+{
+    public interface IAuditNoSqlProvider
+    {
+        /// <summary>
+        /// Gets or sets the connection string.
+        /// </summary>
+        /// <value>The connection string.</value>
+        string ConnectionString { get; set; }
+
+        /// <summary>
+        /// Gets the name of the database.
+        /// </summary>
+        /// <value>The name of the database.</value>
+        string DatabaseEngineName { get; }
+
+        /// <summary>
+        /// Gets the provider namespace.
+        /// </summary>
+        /// <value>The provider namespace.</value>
+        string ProviderNamespace { get; }
+
+        /// <summary>
+        /// Gets the errors from any internally thrown exceptions.
+        /// </summary>
+        /// <value>The errors.</value>
+        List<string> Errors { get; set; }
+    }
+}
