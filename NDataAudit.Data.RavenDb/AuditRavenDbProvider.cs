c646a27d659344ca256e0f0b730484a15f0136ea
Hector Sosa, Jr <hectorsosajr@gmail.com>
2018-05-17 22:31:09 -0600

    Started working on the classes inside the NDataAudit.Data.RavenDb namespace.

---
+++ NDataAudit.Data.RavenDb/AuditRavenDbProvider.cs
@@ -0,0 +1,29 @@
+﻿//*********************************************************************
+// File:       		AuditRavenDbProvider.cs
+// Author:  	    Hector Sosa, Jr
+// Date:			5/17/2018
+//*********************************************************************
+// Change Log
+//*********************************************************************
+// USER					DATE            COMMENTS
+// Hector Sosa, Jr		5/17/2018	    Created
+//*********************************************************************
+
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using Raven.Client;
+
+namespace NDataAudit.Data.RavenDb
+{
+    [Export(typeof(IAuditNoSqlProvider))]
+    public class AuditRavenDbProvider : IAuditNoSqlProvider
+    {
+        public string ConnectionString { get; set; }
+
+        public string DatabaseEngineName { get; }
+
+        public string ProviderNamespace { get; }
+
+        public List<string> Errors { get; set; }
+    }
+}
