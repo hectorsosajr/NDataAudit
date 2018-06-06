0af1c9b7f721988a3daf5ff422a1dd96e5d009cc
Hector Sosa, Jr <hectorsosajr@gmail.com>
2018-06-05 22:05:31 -0600

    Fixed the HTML for the OutputAlert class. It looks quite different from the other ouput object. Anybody with a bit of HML knowledge can change how this looks.

--- NDataAudit/Outputs/OutputAlert.cs
+++ NDataAudit/Outputs/OutputAlert.cs
@@ -32,72 +32,53 @@ namespace NDataAudit.Framework.Outputs
         public override string CreateOutputBody()
         {
             var body = new StringBuilder();
+            string rightNow = DateTime.Now.ToString("yyyy-MM-dd hh:mm:ss tt", CultureInfo.InvariantCulture);
+
             string cleanBody = string.Empty;
-
-            Configuration config = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
-
-            string sourceEmailDescription = config.AppSettings.Settings["sourceEmailDescription"].Value;
 
             foreach (Audit audit in base.Audits)
             {
+                body.Append("<table border=\"1\" width=\"100%\" style=\"border-collapse: collapse;font-family: verdana; font-size: 10pt\">");
+                body.AppendFormat("<tr><td><p><b><font size=\"4\">{0} :: <font color=\"#FF0000\">COMPLETED W/ ERRORS</font></font></b></td></tr>",audit.Name);
+                body.Append("<tr><td width=\"100%\" bgcolor=\"#0B6BE1\"><font color=\"#FFFFFF\"><b>Alert Summary</b></font></td></tr>");
+                body.Append("<tr><td width=\"100%\">");
+                body.Append("<table border=\"1\" width=\"100%\" style=\"border-collapse: collapse;font-family: verdana; font-size: 10pt\">");
+                body.Append("<tr><td width=\"189\" bgcolor=\"#F2F2F2\"><b>Execution Status</b></td><td> <font color=\"#FF0000\">COMPLETED W/ ERRORS</font></td></tr>");
+                body.AppendFormat("<tr><td width=\"189\" bgcolor=\"#F2F2F2\"><b>Ran at </b></td><td>{0}</td></tr>",rightNow);
+                body.AppendFormat("<tr><td width=\"189\" bgcolor=\"#F2F2F2\"><b>Machine Name</b></td><td>{0}</td></tr>",audit.TestServer);
+                body.Append("</table></td></tr></table>");
+                body.Append(AuditUtils.HtmlBreak);
+                body.Append("<table border=\"1\" width=\"100%\" style=\"border-collapse: collapse;font-family: verdana; font-size: 10pt\">");
+                body.Append("<tr><td width=\"100%\" bgcolor=\"#FF0000\"><font color=\"#FFFFFF\" size=\"4\"><b>ERRORS</b></font></td></tr>");
+                body.AppendFormat("<tr><td width=\"100%\" bgcolor=\"#FFD5D5\"><b>Source :</b> {0}</td></tr>",audit.Name);
+                body.AppendFormat("<tr><td width=\"100%\"><b>{0} : </b>{1}</td></tr>",rightNow,audit.Test.FailedMessage);
+                body.Append("</table>");
+                body.Append(AuditUtils.HtmlBreak);
+                body.Append("<table border=\"1\" width=\"100%\" style=\"border-collapse: collapse;font-family: verdana; font-size: 10pt\">");
+                body.Append("<tr><td width=\"100%\" bgcolor=\"#4a781c\"><font color=\"#FFFFFF\" size=\"4\"><b>QUERY</b></font></td></tr>");
+                body.Append("<tr><td width=\"100%\" bgcolor=\"#8dfb9c\"><b>Tested With :</b> </td></tr>");
+                body.AppendFormat("<tr><td width=\"100%\">{0}</td></tr>", audit.Test.SqlStatementToCheck.ToHtml());
+                body.Append("</table>");
+                body.Append(AuditUtils.HtmlBreak);
 
-                if (audit.EmailSubject != null)
+                if (audit.Test.Instructions != null)
                 {
-                    body.AppendLine("<h2>" + audit.EmailSubject + "</h2>");
-                }
-
-                body.Append("This audit ran at " +
-                            DateTime.Now.ToString("yyyy-MM-dd hh:mm:ss tt", CultureInfo.InvariantCulture) +
-                            AuditUtils.HtmlBreak + AuditUtils.HtmlBreak);
-
-                if (audit.ShowThresholdMessage)
-                {
-                    body.AppendLine("<h2>ERROR MESSAGE</h2>");
-                    body.Append(audit.Test.FailedMessage + AuditUtils.HtmlBreak + AuditUtils.HtmlBreak);
-                }
-
-                if (audit.ShowCommentMessage)
-                {
-                    body.AppendLine("COMMENTS AND INSTRUCTIONS" + AuditUtils.HtmlBreak);
-                    body.AppendLine("============================" + AuditUtils.HtmlBreak);
-
-                    if (audit.Test.Instructions != null)
+                    if (audit.Test.Instructions.Length > 0)
                     {
-                        if (audit.Test.Instructions.Length > 0)
-                        {
-                            body.Append(audit.Test.Instructions.ToHtml() + AuditUtils.HtmlBreak);
-                            body.AppendLine(AuditUtils.HtmlBreak);
-                        }
+                        body.Append("<table border=\"1\" width=\"100%\" style=\"border-collapse: collapse;font-family: verdana; font-size: 10pt\">");
+                        body.Append("<tr><td width=\"100%\" bgcolor=\"#6600FF\"><font color=\"#FFFFFF\" size=\"4\"><b>COMMENTS AND INSTRUCTIONS</b></font></td></tr>");
+                        body.Append("<tr><td width=\"100%\" bgcolor=\"#CC99FF\"><b>====</b> </td></tr>");
+                        body.AppendFormat("<tr><td width=\"100%\">{0}</td></tr>",audit.Test.Instructions.ToHtml());
+                        body.Append("</table>");
+                        body.Append(AuditUtils.HtmlBreak);
                     }
                 }
 
-                if (audit.IncludeDataInEmail)
-                {
-                    if (audit.ResultDataSet.Tables.Count > 0)
-                    {
-                        EmailTableTemplate currTemplate = audit.Test.TemplateColorScheme;
-
-                        string htmlData = AuditUtils.CreateHtmlData(audit, audit.ResultDataSet, currTemplate);
-
-                        body.Append(htmlData);
-                    }
-                }
-
-                body.AppendLine(AuditUtils.HtmlBreak);
-
-                body.Append("This alert ran at " + DateTime.Now.ToString("yyyy-MM-dd hh:mm:ss tt", CultureInfo.InvariantCulture) + AuditUtils.HtmlBreak);
-                body.Append("<b>This alert was run on: " + audit.TestServer + "</b>" + AuditUtils.HtmlBreak + AuditUtils.HtmlBreak);
-
-                if (audit.ShowQueryMessage)
-                {
-                    body.Append(AuditUtils.HtmlBreak);
-                    body.Append("The '" + audit.Name + "' audit has failed. The following SQL statement was used to test this alert :" + AuditUtils.HtmlBreak);
-                    body.Append(audit.Test.SqlStatementToCheck.ToHtml() + AuditUtils.HtmlBreak);
-                    body.Append("<b>This query was run on: " + audit.TestServer + "</b>" + AuditUtils.HtmlBreak + AuditUtils.HtmlBreak);
-                }
+                body.Append("<font face=\"Tahoma\" size=\"1\">This alert was generated by <a href=\"https://github.com/hectorsosajr/NDataAudit\">NDataAudit Framework</a></font>");
+                body.Append(AuditUtils.HtmlBreak);
+                body.Append(AuditUtils.HtmlBreak);
 
                 cleanBody = body.ToString().Replace("\r\n", string.Empty);
-
             }
 
             return cleanBody;
